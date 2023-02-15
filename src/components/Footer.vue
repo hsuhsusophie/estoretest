677079d3c5854958dc651919db12a1c9f5f97c5c
hsuhsusophie <hsuhsusophei@gmail.com>
2023-02-15 22:15:45 +0800

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+<v-footer dark padless>
+    <v-card flat tile class="black--text py-12 px-5" acolor="#eeeeee" width="100%">
+        <v-row>
+            <v-col cols="12" sm="4">
+                <v-card-text class="black--text pt-0">
+                    <h3>SHOES GAME</h3>
+                </v-card-text>
+            </v-col>
+        </v-row>
+
+
+    </v-card>
+
+</v-footer>
+ </template>
+    
+    <script>
+    export default{
+    data:() => ({
+        icons:[
+            'mdi-facebook',
+            'mdi-twitter',
+            'mdi-instagram'    ]
+    })
+    
+    }
+    
+    </script>
+    
+    <style>
+    
+    </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+      <Navbar class="index"></Navbar>
+      <Dashboard></Dashboard>
+      <Footer></Footer>
+  </v-app>
+      </template>
+      
+      <script>
+      import Navbar from '../components/Navbar'
+      import Dashboard from '../components/Dashboard'
+      import Footer from '../components/Footer'
+  
+  
+  
+  
+      export default{
+      name: 'Home', 
+      components:{
+          Navbar,
+          Dashboard,
+          Footer
+      },
+      }
+      
+      </script>
+      
+      <style scoped>
+      .index{
+          z-index:9999 ;
+      }
+      
+      </style>
